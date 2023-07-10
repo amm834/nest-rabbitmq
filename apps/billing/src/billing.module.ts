7f1d408afe2177c676d60c3fc0e697953d9c7339
Aung Myat Moe <aungmyatmoe834@gmail.com>
2023-07-11 00:54:08 +0630

    feat: add rabbit mq in the billing service

--- apps/billing/src/billing.module.ts
+++ apps/billing/src/billing.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { BillingController } from './billing.controller';
 import { BillingService } from './billing.service';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import { RmqModule } from '@app/common';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: './apps/billing/.env',
+      validationSchema: Joi.object({
+        RABBIT_MQ_URI: Joi.string().required(),
+        RABBIT_MQ_BILLING_QUEUE: Joi.required(),
+      }),
+    }),
+    RmqModule,
+  ],
   controllers: [BillingController],
   providers: [BillingService],
 })
--- apps/orders/src/main.ts
+++ apps/orders/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,7 +10,8 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get('PORT'));
 }
 
 bootstrap();
