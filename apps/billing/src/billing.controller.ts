45c25bd2a23ec1d3aa9802756e2e9b8013f9a116
Aung Myat Moe <aungmyatmoe834@gmail.com>
2023-07-11 15:43:02 +0630

    feat: ack rabbitmq after the billing process is success

--- apps/billing/src/billing.controller.ts
+++ apps/billing/src/billing.controller.ts
@@ -1,10 +1,14 @@
 import { Controller } from '@nestjs/common';
 import { BillingService } from './billing.service';
 import { Ctx, EventPattern, Payload, RmqContext } from '@nestjs/microservices';
+import { RmqService } from '@app/common';
 
 @Controller()
 export class BillingController {
-  constructor(private readonly billingService: BillingService) {}
+  constructor(
+    private readonly billingService: BillingService,
+    private readonly rmqService: RmqService,
+  ) {}
 
   @EventPattern('order_created')
   async handleOrderCreated(
@@ -12,5 +16,6 @@ export class BillingController {
     @Ctx() context: RmqContext,
   ) {
     this.billingService.bill(data);
+    this.rmqService.ack(context);
   }
 }
